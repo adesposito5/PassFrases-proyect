d5b034f279f06f4e1c6c058b71677a804a5da296
adesposito5 <tu-email@gmail.com>
2026-06-21 20:06:31 -0300

    feat: update router and GeneratorPage for wizard flow

--- src/features/generator/pages/GeneratorPage.tsx
+++ src/features/generator/pages/GeneratorPage.tsx
@@ -1,10 +1,18 @@
-import { AppLayout } from '@/shared/components/ui/AppLayout'
+import { useEffect } from 'react'
+import { usePasswordStore } from '@/features/generator/store'
+import { WizardLayout } from '@/features/generator/components/WizardLayout'
 import GeneratorPanel from '@/features/generator/components/GeneratorPanel'
 
 export default function GeneratorPage() {
+  const setStep = usePasswordStore((state) => state.setStep)
+
+  useEffect(() => {
+    setStep(2)
+  }, [setStep])
+
   return (
-    <AppLayout>
+    <WizardLayout>
       <GeneratorPanel />
-    </AppLayout>
+    </WizardLayout>
   )
 }
